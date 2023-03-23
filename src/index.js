7dbfefae0e6486bd76e2f775a1f3dd48f82f7929
Bruce <bruce.hillsworth@gmail.com>
2023-03-23 14:32:14 -0600

    Add submitTask() and EventListener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newTaskSubmitBtn = document.querySelector("#new-task-submit");
+  const taskList = document.querySelector("#tasks");
+
+  function submitTask() {
+    event.preventDefault();
+    const taskDescription = document.querySelector("#new-task-description");
+    const toDo = taskDescription.value;
+    const newTask = document.createElement("li");
+    newTask.textContent = toDo;
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Remove Task";
+    newTask.appendChild(deleteButton);
+    taskList.appendChild(newTask);
+    taskDescription.value = "";
+    deleteButton.addEventListener("click", () => {
+      newTask.remove();
+      deleteButton.remove();
+    });
+  }
+
+  newTaskSubmitBtn.addEventListener("click", submitTask);
 });
